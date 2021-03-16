b47b11b8e69b237ec4b76327303305f13a4f991d
Robert Franklin <rcf@mince.net>
2021-03-16 23:21:07 +0000

    added deepsetdefault()
    this function is like dict.setdefault() except it can add a number of
    levels to a dictionary structure, like stringing together multiple
    calls to that function

--- deepops/deepops.py
+++ deepops/deepops.py
@@ -41,6 +41,7 @@ class DeepPath(list):
 
         return "".join(map(lambda i: "[%s]" % repr(i), self)) or "<root>"
 
+
     def sub(self, sub_item):
         """Returns a new path, extended by the supplied sub item.  The
         returned path is a copy and does not affect the path the
@@ -51,6 +52,7 @@ class DeepPath(list):
         """
 
         return DeepPath(self + [sub_item])
+
 
     def startswith(self, test_path):
         """Returns whether the path starts with (i.e. is contained
@@ -684,3 +686,51 @@ def deepdiff(a, b, list_as_set=False, change_types=False, filter_func=None):
     """
 
     return _deepdiff(a, b, list_as_set, change_types, filter_func)
+
+
+
+# deepsetdefault
+
+
+
+def deepsetdefault(d, *path, last={}):
+    """This function performs a path of dict.setdefault() calls to
+    initialise multiple levels of a dictionary 'd' with a number of path
+    elements ('path', as a variable argument list).
+
+    For example, 'deepsetdefault({}, 1, 2)' will give a dictionary of
+    {1: { 2: {} }}.
+
+    The optional 'last' argument allows the final object to be changed
+    from a dict().
+
+    As with setdefault(), the final object added will be returned.
+    """
+
+
+    # get the length of the path so we can count down until the last
+    # item
+
+    path_remain = len(path)
+
+
+    # start at the top of the dictionary
+
+    d_sub = d
+
+    for key in path:
+        # setdefault() the next level in the path, using the last
+        # parameter, if this is the final level - if so, we also need to
+        # copy() it first, otherwise the same instance will be used for
+        # all defaults (if it was the default argument)
+
+        d_sub = d_sub.setdefault(
+            key, {} if path_remain > 1 else last.copy())
+
+
+        path_remain -= 1
+
+
+    # return where we got to at the end of the path
+
+    return d_sub
